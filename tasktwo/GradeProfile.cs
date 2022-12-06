46cd6f36708ffe02f1885765a30f937607cb1fba
Hussnain <Hussnain@LAPTOP-GT3BOFE7>
2022-12-06 13:11:51 +0000

    added more grades

--- tasktwo/GradeProfile.cs
+++ tasktwo/GradeProfile.cs
@@ -9,48 +9,31 @@ namespace tasktwo
 {
     class GradeProfile
     {
-        public static void calcAvg()
+        public static void CalcAvg()
         {
 
-
-            double av1 = Program.student1.grades.Average(item => item.Mark);
-            double av2 = Program.student2.grades.Average(item => item.Mark);
+            double avg1 = Program.student1.grades.Average(item => item.Mark);
+            double avg2 = Program.student2.grades.Average(item => item.Mark);
 
             WriteLine($"Student ID: {Program.student1.StudentId}\n Name: {Program.student1.Name}\n Course: {Program.student1.Course}");
+
             foreach (Grades g in Program.student1.grades)
             {
                 Console.WriteLine($"Module: {g.Module}\n Assignment: {g.Assignment}\n Grade: {g.Mark}\n Year: {g.Year}\n");
 
             }
-            WriteLine($" Average Grade: {av1}\n");
+
+            WriteLine($" Average Grade: {avg1}\n");
 
 
             WriteLine($"Student ID: {Program.student2.StudentId}\n Name: {Program.student2.Name}\n Course: {Program.student2.Course}");
+
             foreach (Grades g in Program.student2.grades)
             {
                 Console.WriteLine($"Module: {g.Module}\n Assignment: {g.Assignment}\n Grade: {g.Mark}\n Year: {g.Year}\n ");
 
             }
-            WriteLine($" Average Grade: {av2}");
-
-
-
-            /*
-            int total = Student.grades.Sum(x => x);
-            Console.WriteLine(total);
-            int gradeone = Student.grades[0];
-            int gradetwo = Student.grades[1];
-
-            //double average = Student.grades.Average();
-            //Console.WriteLine($"Average: {average}");
-
-
-            
-             foreach (var g in Grades.grades)
-            {
-                Console.WriteLine(total);
-            }
-          */
+            WriteLine($" Average Grade: {avg2}");
         }
     }
 }
--- tasktwo/Program.cs
+++ tasktwo/Program.cs
@@ -8,26 +8,30 @@ namespace tasktwo
 {
     class Program
     {
+        //Adding students to the Student class
         public static Student student1 = new(1, "Hussnain", "Computing");
-        public static Student student2 = new(2, "Zaf", "Maths");
+        public static Student student2 = new(2, "Ben", "Maths");
         public static void Main(string[] args)
         {
-            
-            
-
+            //Student 1 grades
             Grades grade1 = new(1, "OOP", "Assignment 010", 40, 2022);
-            Grades grade2 = new(1, "Networking", "Assignment 023", 50, 2022);
+            Grades grade2 = new(1, "Networking", "Assignment 023", 60, 2022);
+            Grades grade3 = new(1, "Interactive Web", "Assignment 024", 50, 2022);
 
-            Grades grade3 = new(2, "Matrices", "Assignment 010", 60, 2022);
-            Grades grade4 = new(2, "Algebra", "Assignment 023", 70, 2022);
+            //Student 2 grades
+            Grades grade4 = new(2, "OOP", "Assignment 010", 60, 2022);
+            Grades grade5 = new(2, "Networking", "Assignment 023", 70, 2022);
+            Grades grade6 = new(2, "Maths", "Assignment 024", 50, 2022);
 
             student1.grades.Add(grade1);
             student1.grades.Add(grade2);
+            student1.grades.Add(grade3);
 
-            student2.grades.Add(grade2);
-            student2.grades.Add(grade3);
+            student2.grades.Add(grade4);
+            student2.grades.Add(grade5);
+            student2.grades.Add(grade6);
             // variable that calculate the grade average of a student
-            GradeProfile.calcAvg();
+            GradeProfile.CalcAvg();
             
 
         }
